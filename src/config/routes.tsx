ec2be967d15810cbdb052a380176ed2539a95e63
Daniel Eze <danieleze@ced.local>
2019-04-13 14:26:17 +0100

    Setup SCSS for the project :wrench: :ghost:§

---
+++ src/config/routes.tsx
@@ -0,0 +1,11 @@
+import Test from '../components/Test';
+
+const routes = [
+  {
+    path: '/',
+    component: Test,
+    name: 'signup',
+    exact: true,
+  }
+];
+export default routes;
--- src/containers/Root/index.tsx
+++ src/containers/Root/index.tsx
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import routes from '../../config/routes';
 import './root.scss';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello
-      </div>
+      <Router>
+        <Switch>
+          {
+            routes.map(route => 
+              <Route
+                path={route.path}
+                key={route.name}
+                component={route.component}
+              />
+              )
+          }
+        </Switch>
+      </Router>
     );
   }
 }
