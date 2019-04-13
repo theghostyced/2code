1f58b80f596031cb42640a0390b3238bb9c10cac
Daniel Eze <danieleze@ced.local>
2019-04-13 12:57:23 +0100

    Inital Commit :ghost:

---
+++ src/containers/Root/index.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './root.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    );
+  }
+}
+
+export default App;
